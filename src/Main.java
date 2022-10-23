b314f5d29772e5c18600e1fd648c59bd9db26870
Rumonz <lone_assassin@mail.ru>
2022-10-23 13:57:03 +0300

    выполнил задание 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //задание 1
+        int i = 2147483647;
+        System.out.println("Значение переменной i с типоv int равно " + i);
+        byte b = 127;
+        System.out.println("Значение переменной b с типом byte равно "+ b);
+        short s = 32767;
+        System.out.println("Значение переменной s с типом short равно " +s);
+        long l = 9223372036854775807L;
+        System.out.println("Значение переменной l с типом long равно "+l);
+        float f = 3.14159265F;
+        System.out.println("Значение переменной f с типом float равно "+f);
+        double d = 3.14159265358979323846264;
+        System.out.println("Значение переменной d с типом double равно "+d);
+    }
+}
